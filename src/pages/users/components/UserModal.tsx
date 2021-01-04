38f47024a4c3ecb945008b998a60bb4f80321dd8
heyanyan <2469191878@qq.com>
2021-01-04 21:04:08 +0800

    初始化项目

--- src/pages/users/components/UserModal.tsx
+++ src/pages/users/components/UserModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Button } from 'antd';
+import { Modal, Button, Form, Input } from 'antd';
 
 const UsersModel = (props: any) => {
   // @ts-ignore
@@ -11,7 +11,32 @@ const UsersModel = (props: any) => {
         onOk={props.closeHandler}
         onCancel={props.closeHandler}
       >
-        {JSON.stringify(props.record)}
+        <Form name="basic">
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your name!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Email" name="email">
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Create Time" name="create time">
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Status" name="status">
+            <Input />
+          </Form.Item>
+        </Form>
       </Modal>
     </div>
   );
